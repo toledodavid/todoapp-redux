e8f37b2a5341dbd7be6226d4593ced24eeaaa308
David Toledo <dtoledoz0207@gmail.com>
2020-05-27 11:16:01 -0500

    show-editForm-todoItem

--- src/app/todos/todo-item/todo-item.component.ts
+++ src/app/todos/todo-item/todo-item.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
+import { Todo } from '../models/todo.model';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-todo-item',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoItemComponent implements OnInit {
 
+  @Input() todo: Todo;
+  @ViewChild('inputFisico', {static: false}) txtInputFisico: ElementRef;
+
+  chkCompletado: FormControl;
+  txtEdit: FormControl;
+
+  editando: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
+    this.chkCompletado = new FormControl(this.todo.completado);
+    this.txtEdit = new FormControl(this.todo.text, Validators.required);
+  }
+
+  editar() {
+    this.editando = true;
+    setTimeout(() => {
+      this.txtInputFisico.nativeElement.select();
+    }, 1);
+  }
+
+  terminarEdicion() {
+    this.editando = false;
   }
 
 }
