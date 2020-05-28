ecff5c8bbf0a341f830fc36a896c065f71149c4b
David Toledo <dtoledoz0207@gmail.com>
2020-05-28 12:01:10 -0500

    add-dispatch-establecerFiltroAction-to-set-new-filtro-in-state

--- src/app/todos/todo-footer/todo-footer.component.ts
+++ src/app/todos/todo-footer/todo-footer.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/app.reducer';
+import { filtrosValidos, establecerFiltro } from 'src/app/filtro/filtro.actions';
 
 @Component({
   selector: 'app-todo-footer',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoFooterComponent implements OnInit {
 
-  constructor() { }
+  filtroActual: filtrosValidos;
+  filtros: filtrosValidos[] = ['todos', 'completados', 'pendientes'];
+
+  constructor(private store: Store<AppState>) { }
 
   ngOnInit() {
+    this.store.select('filtro').subscribe(filtro => this.filtroActual = filtro);
+  }
+
+  cambiarFiltro(filtro:filtrosValidos) {
+    this.store.dispatch(establecerFiltro({filtro: filtro}));
   }
 
 }
